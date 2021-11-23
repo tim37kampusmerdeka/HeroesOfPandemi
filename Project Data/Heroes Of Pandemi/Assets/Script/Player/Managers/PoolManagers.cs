989ce0939983290a8387a67ce2944bf8d7c605d6
Rama <miqra1612@gmail.com>
2021-11-23 09:29:08 +0700

    - adding player script

---
+++ Project Data/Heroes Of Pandemi/Assets/Script/Player/Managers/PoolManagers.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoolManagers : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    public int spawnCount = 7;
+    public List<GameObject> bulletList;
+    void Start()
+    {
+        for (int i = 0; i < spawnCount; i++)
+        {
+            GameObject bullet = Instantiate(bulletPrefab) as GameObject;
+            bulletList.Add(bullet);
+            bullet.transform.parent = this.transform;
+            bullet.SetActive(false);
+        }
+    }
+}
