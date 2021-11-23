33deebd42a7f70bae0b35e8281581b9930e5d8c7
Alfaa719 <nurul.hidayat.f3@gmail.com>
2021-11-24 01:57:05 +0700

    Add Player Health, HealthBar Prefab, OnPlayerDead Condition

---
+++ Project Data/Heroes Of Pandemi/Assets/Script/Player/Player/PlayerHealthSystem.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealthSystem : MonoBehaviour
+{
+    public GameObject healthbarPrefab;
+    public int maxHealth = 100, health;
+    public Slider healthSlider;
+    public Text barText;
+
+    void Start()
+    {
+        // Set Health, text bar dan slider
+        health = maxHealth;
+        healthSlider.value = health;
+        barText.text = health + "/" + maxHealth;
+
+        //Invoke void trydamage
+        InvokeRepeating("TryDamage", 5, 0.1f);
+    }
+    void OnPlayerDead(bool dead = false)
+    {
+        if (dead)
+        {
+            this.gameObject.SetActive(false);
+            healthbarPrefab.SetActive(false);
+        }
+    }
+    void TakeDamage(int amount)
+    {
+        health -= amount;
+        healthSlider.value = health;
+        barText.text = health + "/" + maxHealth;
+        if (health <= 0)
+        {
+            OnPlayerDead(true);
+        }
+    }
+    //Test Damage
+    void TryDamage()
+    {
+        TakeDamage(1);
+    }
+
+}
